0b8bbe675f5d56b104269ac4387c7c22c3562784
Andrew Mitchell <andrewjmitchell2000@gmail.com>
2019-04-06 13:11:07 -0400

    Added twitter api

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require("express");
-const path = require("path");
+var Twitter = require('twitter');
+
+// Load environment variables
+require('dotenv').config();
 
 const app = express();
 
 const port = 3000;
 const appVersion = require('./package.json').version;
 
+var twitter = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-    res.sendFile("index.html")
+    res.sendFile("index.html");
 });
+
+twitter.stream('statuses/filter', {locations: "-84.217003,39.727303,-83.937769,39.863267"},  function(stream) {
+    stream.on('data', function(tweet) {
+      console.log(tweet.text);
+    });
+  
+    stream.on('error', function(error) {
+      console.log(error);
+    });
+  });
 
 app.listen(port, () => console.log(`Created by Andrew Mitchell, Jacob Hackman, Kainan Woodard.\nRunning Geoposts v` + appVersion + ` on port ${port} in ${app.settings.env} mode!`));
