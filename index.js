32cf145887030d276a6a2a6b6156ecede3ebbb8c
Andrew Mitchell <andrewjmitchell2000@gmail.com>
2019-04-06 10:51:19 -0400

    Add more info to console messages

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 
 const port = 3000;
+const appVersion = require('./package.json').version;
 
 app.get('/', function(req, res) {
     res.send("Hello World!");
 });
 
-app.listen(port, () => console.log(`Geoposts listening on port ${port}!`));
+app.listen(port, () => console.log(`Created by Andrew Mitchell, Jacob Hackman, Kainan Woodard.\nRunning Geoposts v` + appVersion + ` on port ${port} in ${app.settings.env} mode!`));
