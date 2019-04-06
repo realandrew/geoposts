a675527daa5acc182479582fa6b48b507a9d2966
Andrew Mitchell <andrewjmitchell2000@gmail.com>
2019-04-06 19:55:09 -0400

    Better console log messages

--- public/js/main.js
+++ public/js/main.js
@@ -25,8 +25,8 @@ var map = new ol.Map({
 
 var socket = io();
 socket.on("new precise tweet", function(msg) {
-    console.log(msg.data);
-    console.log("0: " + msg.data[0] + " 1: " + msg.data[1]);
+    //console.log(msg.data);
+    console.log("New tweet from: long: " + msg.data[0] + " lat: " + msg.data[1]);
     var marker2 = new ol.Feature({
         geometry: new ol.geom.Point(
             ol.proj.fromLonLat([msg.data[0], msg.data[1]])
@@ -36,10 +36,10 @@ socket.on("new precise tweet", function(msg) {
 });
 
 socket.on("new loose tweet", function(msg) {
-    console.log(msg.data);
-    console.log("000: " + msg.data[0][0][0] + " 001: " + msg.data[0][0][1] + " 010: " + msg.data[0][1][0] + " 011: " + msg.data[0][1][1]);
+    //console.log(msg.data);
+    console.log("New tweet within area sw-long: " + msg.data[0][0][0] + " sw-lat: " + msg.data[0][0][1] + " ne-long: " + msg.data[0][1][0] + " ne-lat: " + msg.data[0][1][1]);
     let geodata = [((msg.data[0][0][0] + msg.data[0][1][0]) / 2), ((msg.data[0][0][1] + msg.data[0][1][1]) / 2)];
-    console.log("g0: " + geodata[0] + " g1: " + geodata[1]);
+    console.log("Area midpoint: long: " + geodata[0] + " lat: " + geodata[1]);
     var marker2 = new ol.Feature({
         geometry: new ol.geom.Point(
             ol.proj.fromLonLat([geodata[0], geodata[1]])
