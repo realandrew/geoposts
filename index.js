877a141a1214ab4f85e3cd3933c6e8f6a254b85f
Andrew Mitchell <andrewjmitchell2000@gmail.com>
2019-04-06 11:06:37 -0400

    Setup static file serving

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+
 const app = express();
 
 const port = 3000;
 const appVersion = require('./package.json').version;
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
-    res.send("Hello World!");
+    res.sendFile("index.html")
 });
 
 app.listen(port, () => console.log(`Created by Andrew Mitchell, Jacob Hackman, Kainan Woodard.\nRunning Geoposts v` + appVersion + ` on port ${port} in ${app.settings.env} mode!`));
